cb67e2f08b5a04d9a9cf679412ea70532522fdec
Minhaj <minhaj_tahir@aptechnorth.edu.pk>
2026-08-29 17:09:04 +0500

    Add db:import-dump artisan command for easy migration

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,19 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('db:import-dump', function () {
+    $path = base_path('eventsphere_database_backup.sql');
+    if (!file_exists($path)) {
+        $this->error("Backup file not found at: {$path}");
+        return 1;
+    }
+    
+    $this->info("Importing eventsphere_database_backup.sql into database...");
+    
+    $sql = file_get_contents($path);
+    \Illuminate\Support\Facades\DB::unprepared($sql);
+    
+    $this->info("Database imported successfully with all data!");
+    return 0;
+})->purpose('Import the backup SQL dump file into the database');
